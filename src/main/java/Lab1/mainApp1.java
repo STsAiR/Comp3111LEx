ca5b3d63fcb5d66620d3e8e6ec7fbfef19b7d331
STsAiR <94659937+STsAiR@users.noreply.github.com>
2023-09-11 18:07:36 +0800

    Fixing gitignore

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -7,7 +7,7 @@ public class mainApp1 {
         System.out.println("Welcome to Scientific Calculator!");
         System.out.println("Program started ...");
         System.out.println(b + " to power "+n+" = "+ myLibrary.Power(b,n));
-        System.out.println(n+"! = "+ myLibrary.factorial(n));
+        System.out.println(n+ "! = "+ myLibrary.factorial(n));
         System.out.println("Program ..Ended ...");
     }
 }
